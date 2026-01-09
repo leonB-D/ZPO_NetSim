a136f11eae2f18f8abddf828492e461e31ef0eb6
student141 <student141@autolab2.local>
2026-01-09 12:52:11 +0100

    Package.hxx prob done

--- include/package.hxx
+++ include/package.hxx
@@ -2,7 +2,23 @@
 
 class Package {
 public:
-    Package(ElementID);
-    Package(Package&&);
-    ElementID get_id();
+
+    Package();
+    explicit Package(ElementID id) : id_(id) {}
+    Package(Package&& other) noexcept : id_(other.id_) {
+        other.id_ = 0;
+    }
+    Package& operator=(Package&& other) noexcept;
+
+
+    Package(const Package&) = delete;
+    Package& operator=(const Package&) = delete;
+
+    ElementID get_id() const { return id_; }
+
+    ~Package() = default;
+
+private:
+    ElementID id_;
+
 };
