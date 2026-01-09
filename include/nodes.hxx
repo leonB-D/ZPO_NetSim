3c61ff6c2382b4574fe9f281fcf464b53b10aef1
Leon <leon.pisarik@gmail.com>
2026-01-09 09:11:10 +0100

    przed zajeciami

--- include/nodes.hxx
+++ include/nodes.hxx
@@ -25,7 +25,7 @@ class ReceiverPreferences {
 public:
     preferences_t preferences_;
 
-    ReceiverPreferences(ProbabilityGenerator pg);
+    explicit ReceiverPreferences(ProbabilityGenerator pg);
     void add_receiver(IPackageReceiver* r);
     void remove_receiver(IPackageReceiver* r);
     IPackageReceiver* choose_receiver();
@@ -37,14 +37,20 @@ public:
     ReceiverPreferences receiver_preferences_;
 
     PackageSender(PackageSender&&) = default;
+
     void send_package();
     std::optional<Package>& get_sending_buffer();
 protected:
-    void push_package(Package&&);
+    std::optional<Package> buffer = std::nullopt;
+
+    void push_package(Package&& p);
 };
 
 class Ramp : public PackageSender {
 public:
+    ElementID id_;
+    TimeOffset time_offset_;
+
     Ramp(ElementID id, TimeOffset di);
     void deliver_goods(Time t);
     TimeOffset get_delivery_interval();
@@ -53,6 +59,10 @@ public:
 
 class Worker : public PackageSender {
 public:
+    ElementID id_;
+    TimeOffset time_offset_;
+    std::unique_ptr<IPackageQueue> queue_;
+
     Worker(ElementID id, TimeOffset pd, std::unique_ptr<IPackageQueue> q);
     void do_work(Time t);
     TimeOffset get_processing_duration();
@@ -61,6 +71,9 @@ public:
 
 class Storehouse {
 public:
+    ElementID id_;
+    std::unique_ptr<IPackageQueue> queue_;
+
     Storehouse(ElementID id, std::unique_ptr<IPackageQueue> d);
 };
 
--- src/nodes.cpp
+++ src/nodes.cpp
@@ -1,1 +1,18 @@
 #include "nodes.hxx"
+
+Ramp::Ramp(ElementID id, TimeOffset di)
+    : PackageSender(this&), id_(id), time_offset_(di) {}
+
+Worker::Worker(ElementID id, TimeOffset pd, std::unique_ptr<IPackageQueue> q)
+    : PackageSender(this&), id_(id), time_offset_(pd), queue_(std::move(q)) {}
+
+Storehouse::Storehouse(ElementID id, std::unique_ptr<IPackageQueue> d)
+    : id_(id), queue_(std::move(d)) {}
+
+void PackageSender::push_package(Package&& p) {
+    this->buffer = p;
+}
+
+void PackageSender::send_package() {
+    this->buffer = std::nullopt;
+}
