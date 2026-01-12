70c96b6b9b8dad19b605b036be84681d93873e48
kpastula <kacperpastula4@gmail.com>
2026-01-12 11:56:05 +0100

    PackageQueue Interface

--- include/storage_types.hxx
+++ include/storage_types.hxx
@@ -21,12 +21,51 @@ public:
     virtual const_iterator cend() const = 0;
 
 };
+
 class IPackageQueue: public IPackageStockpile {
 public:
     ~IPackageQueue() override = default;
     virtual Package pop() = 0;
     virtual PackageQueueType get_QueueType() const = 0;
 };
-class PackageQueue {
 
+class PackageQueue: public IPackageQueue {
+public:
+    PackageQueue() = delete;
+    ~PackageQueue() override = default;
+    explicit PackageQueue(PackageQueueType queueType): package_QueueType_(queueType), package_list_() {};
+
+    void push(Package&& package) override {
+    this->package_list_.emplace_back(std::move(package));
+    }
+    bool empty() const override {
+    return this->package_list_.empty();
+    }
+    size_t size() const override{
+        return this->package_list_.size();
+    }
+    const_iterator begin() const override
+    {
+        return this->package_list_.cbegin();
+    }
+    const_iterator end() const override
+    {
+        return this->package_list_.cend();
+    }
+    const_iterator cbegin() const override
+    {
+        return this->package_list_.cbegin();
+    }
+    const_iterator cend() const override
+    {
+        return this->package_list_.cend();
+    }
+    Package pop() override;
+    PackageQueueType get_QueueType() const override
+    {
+        return this->package_QueueType_;
+    };
+private:
+    PackageQueueType package_QueueType_;
+    std::list<Package> package_list_;
 };
