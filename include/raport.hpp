666b7a776de9556f84322aaa2c49d03ac5bb6a5e
kpastula <kacperpastula4@gmail.com>
2026-01-13 14:17:13 +0100

    Added raport source and include files

---
+++ include/raport.hpp
@@ -0,0 +1,14 @@
+//
+// Created by Kacper on 13.01.2026.
+//
+#ifndef RAPORT_HPP
+#define RAPORT_HPP
+
+#include "factory.hxx"
+#include <iostream>
+
+void generate_structure_report(const Factory& f,std::ostream& os);
+void generate_simulation_turn_report(const Factory& f,std::ostream& os,Time t);
+
+#endif //RAPORT_HPP
+
---
+++ src/raport.cpp
@@ -0,0 +1,3 @@
+//
+// Created by Kacper on 13.01.2026.
+//
